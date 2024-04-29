e40049c6f536544655ea90ce7a1704a3c865d082
eddy <adigupta239@gmail.com>
2024-04-29 13:41:53 +0530

    changed styling for socials

--- app/page.jsx
+++ app/page.jsx
@@ -10,19 +10,19 @@ const Home = () => {
       </div>
       <div className="flex gap-3">
         <a href="https://github.com/eddy1006" target="_blank">
-          <div className="bg-githubColor  opacity-30 h-12 w-12 hover:opacity-100"></div>
+          <div className="bg-githubColor  opacity-30 h-12 w-12 hover:opacity-100 transform transition duration-500 hover:scale-125"></div>
         </a>
         <a
           href="https://www.linkedin.com/in/aditya-gupta-0985b621a/"
           target="_blank"
         >
-          <div className="bg-linkedinColor opacity-30 h-12 w-12 hover:opacity-100"></div>
+          <div className="bg-linkedinColor opacity-30 h-12 w-12 hover:opacity-100 transform transition duration-500 hover:scale-125"></div>
         </a>
         <a href="https://twitter.com/eddityyaa" target="_blank">
-          <div className="bg-xColor opacity-30 h-12 w-12 hover:opacity-100"></div>
+          <div className="bg-xColor opacity-30 h-12 w-12 hover:opacity-100 transform transition duration-500 hover:scale-125"></div>
         </a>
         <a href="https://leetcode.com/adigupta239/" target="_blank">
-          <div className="bg-leetcodeColor opacity-30 h-12 w-12 hover:opacity-100"></div>
+          <div className="bg-leetcodeColor opacity-30 h-12 w-12 hover:opacity-100 transform transition duration-500 hover:scale-125"></div>
         </a>
       </div>
     </div>
